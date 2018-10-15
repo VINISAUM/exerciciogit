b050a67c49381bcfae0acff37741abff72001926
Vinicius <vinicius1941@hotmail.com>
2018-10-15 20:25:48 -0300

    Modificado o arquivo functions.php. Adicionado funtion outraFuncao() e novaFuncao()

--- functions.php
+++ functions.php
@@ -2,4 +2,10 @@
 	function umaFuncao() {
 		echo "Uma função que imprime".PHP_EOL;
 	}
+	function outraFuncao($msg) {
+		echo "Sou uma funcao: {$msg}".PHP_EOL;
+	}
+	function novaFuncao($msg) {
+		echo "Sou uma funcao que recebe: {$msg}".PHP_EOL;
+	}
 ?>
